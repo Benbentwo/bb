cf0050832a3c4cf9d0e809bf7cab0d0d3677458c
Benjamin Smith <Benjamin.Smith@ablevets.com>
2019-10-02 12:29:06 -0400

    running the binary just gives the help command

--- pkg/cmd/cmd.go
+++ pkg/cmd/cmd.go
@@ -4,19 +4,54 @@ import (
 	"github.ablevets.com/Digital-Transformation/av/pkg/log"
 	"github.com/jenkins-x/jx/pkg/cmd/clients"
 	"github.com/jenkins-x/jx/pkg/cmd/opts"
+	"github.com/jenkins-x/jx/pkg/cmd/templates"
+	"github.com/jenkins-x/jx/pkg/extensions"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"gopkg.in/AlecAivazis/survey.v1/terminal"
 	"io"
+	"os"
 	"strconv"
+	"strings"
 )
 
 func NewAVCommand(f clients.Factory, in terminal.FileReader, out terminal.FileWriter, err io.Writer, args []string) *cobra.Command {
+	replacer := strings.NewReplacer("-", "_")
+	viper.SetEnvKeyReplacer(replacer)
 	cmds := &cobra.Command{
 		Use:              "av",
 		Short:            "AV CLI tool and utility",
 		PersistentPreRun: setLoggingLevel,
 		Run:              runHelp,
 	}
+	//log.Logger().Debugf("running %s", cmds.CalledAs())
+	commonOpts := opts.NewCommonOptionsWithTerm(f, in, out, err)
+	commonOpts.AddBaseFlags(cmds)
+	if len(args) == 0 {
+		args = os.Args
+	}
+	if len(args) > 1 {
+		cmdPathPieces := args[1:]
+
+		if _, _, err := cmds.Find(cmdPathPieces); err != nil {
+			log.Logger().Errorf("%v", err)
+			os.Exit(1)
+		}
+	}
+	groups := templates.CommandGroups{}
+	getPluginCommandGroups := func() (templates.PluginCommandGroups, bool) {
+		verifier := &extensions.CommandOverrideVerifier{
+			Root:        cmds,
+			SeenPlugins: make(map[string]string, 0),
+		}
+		pluginCommandGroups, managedPluginsEnabled, err := commonOpts.GetPluginCommandGroups(verifier)
+		if err != nil {
+			log.Logger().Errorf("%v", err)
+		}
+		return pluginCommandGroups, managedPluginsEnabled
+	}
+
+	templates.ActsAsRootCommand(cmds, []string{"options"}, getPluginCommandGroups, groups...)
 	return cmds
 }
 
